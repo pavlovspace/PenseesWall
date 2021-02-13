bffd59ae551b89cfec0d483fe4143f9c27199349
Paweł Pavlov <pavlov1space@gmail.com>
2021-02-13 13:49:33 +0100

    implemented Hashing Password
    Using bcrypt js

--- server/app.js
+++ server/app.js
@@ -1,15 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const authRoutes = require('./routes/auth')
 const app = express()
-const mongoose  = require('mongoose')
-const PORT = 5000
-const {MONGOURI} = require('./keys')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 5000
 
-require('./models/user')
-
-mongoose.connect(MONGOURI,{
+mongoose.connect('mongodb+srv://Paul:mwm4hADVl6M7UX9g@cluster0.vm2tr.mongodb.net/<dbname>?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology: true
-
 })
 mongoose.connection.on('connected',()=>{
     console.log("conneted to mongo yeahh")
@@ -18,9 +16,11 @@ mongoose.connection.on('error',(err)=>{
     console.log("err connecting",err)
 })
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use('/api/auth', authRoutes)
 
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
 })
-
--- server/keys.js
+++
@@ -1,3 +0,0 @@
-module.export={
-    MONGOURI:"mongodb+srv://penseesWall:<password>@cluster0.doulc.mongodb.net/<dbname>?retryWrites=true&w=majority"
-}
