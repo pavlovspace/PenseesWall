a152474cc14fc8988e9e8994d1b9966c819e1a3a
Paweł Pavlov <pavlov1space@gmail.com>
2021-02-14 17:46:05 +0100

    implement Sending token using JWT(jsonwebtoken)
    Creating Sign in/Login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,18 +4,19 @@ const mongoose = require('mongoose')
 var User = require('../models/User')
 const crypto = require('crypto')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = 'somthjing'
 
-
-
+// Signup route
 router.post('/signup',(req,res)=>{
     const {name,email,password} = req.body 
     if(!email || !password || !name){
-       return res.status(422).json({error:"please add all the fields"})
+       return res.status(402).json({error:"please add all the fields"})
     }
     User.findOne({email:email})
     .then((savedUser)=>{
         if(savedUser){
-          return res.status(422).json({error:"user already exists with that email"})
+          return res.status(402).json({error:"user already exists with that email"})
         }
         //Hashing Password
         bcrypt.hash(password,12)
@@ -39,6 +40,34 @@ router.post('/signup',(req,res)=>{
     .catch(err=>{
       console.log(err)
     })
+})
+
+// Sign in/Login route
+router.post('/signin',(req,res)=>{
+  const {email,password} = req.body
+  if(!email || !password){
+     return res.status(402).json({error:"please add email or password"})
+  }
+  User.findOne({email:email})
+  .then(savedUser=>{
+      if(!savedUser){
+         return res.status(402).json({error:"Invalid Email or password"})
+      }
+      bcrypt.compare(password,savedUser.password)
+      .then(doMatch=>{
+          if(doMatch){
+             const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
+             res.json({token})
+          }
+          else{
+              return res.status(422).json({error:"Invalid Email or password"})
+          }
+      })
+      .catch(err=>{
+          console.log(err)
+      })
   })
+})
+
 
 module.exports = router
