c057bcacdf380e1d5d193c34ff4e300066a21bfb
Paweł Pavlov <pavlov1space@gmail.com>
2021-02-15 04:18:25 +0100

    implemented all post created by signed In user route
    implemented View all posts route,
    All post created by signed In user route

--- server/routes/post.js
+++ server/routes/post.js
@@ -5,6 +5,27 @@ const requireLogin  = require('../middleware/requireLogin')
 const Post = mongoose.model("Post") 
 // var Post = require('../models/Post')
 
+// View all posts route
+router.get('/allpost', (req,res)=>{
+    // Post.find()
+    // .populate("postedBy","_id name")
+    // .populate("comments.postedBy","_id name")
+    // .sort('-createdAt')
+    // .then((posts)=>{
+    //     res.json({posts})
+    // }).catch(err=>{
+    //     console.log(err)
+    // })
+    Post.find()
+    .populate("postedBy","_id name")
+    .then(posts=>{
+        res.json({posts})
+    })
+    .catch(err=>{
+            console.log(err)
+    })
+})
+
 
 router.post('/createpost',requireLogin,(req,res)=>{
     const {title,body} = req.body 
@@ -19,6 +40,18 @@ router.post('/createpost',requireLogin,(req,res)=>{
     })
     post.save().then(result=>{
         res.json({post:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+// All post created by signed In user, MYPOST
+router.get('/mypost',requireLogin,(req,res)=>{
+    Post.find({postedBy:req.user._id})
+    .populate("PostedBy","_id name")
+    .then(mypost=>{
+        res.json({mypost})
     })
     .catch(err=>{
         console.log(err)
