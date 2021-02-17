384c9f4762829bfb3f3a183d8a728895ba3a4bec
Paweł Pavlov <pavlov1space@gmail.com>
2021-02-17 01:25:18 +0100

    implemented upload image to cloudinary

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/screens/Home'
 import Signin from './components/screens/SignIn'
 import Profile from './components/screens/Profile'
 import Signup from './components/screens/Signup'
+import CreatePost from './components/screens/CreatePost'
 
 
 
@@ -25,6 +26,9 @@ function App(){
     </Route>
     <Route path="/signup">
         <Signup />
+    </Route>
+    <Route path="/create">
+        <CreatePost/>
     </Route>
   </BrowserRouter>
   ); 
--- server/routes/post.js
+++ server/routes/post.js
@@ -28,14 +28,15 @@ router.get('/allpost', (req,res)=>{
 
 
 router.post('/createpost',requireLogin,(req,res)=>{
-    const {title,body} = req.body 
-    if(!title || !body ){
-      return  res.status(402).json({error:"Plase add all the fields"})
+    const {title,body,pic} = req.body 
+    if(!title || !body || !pic){
+      return  res.status(422).json({error:"Plase add all the fields"})
     }
     req.user.password = undefined
     const post = new Post({
         title,
         body,
+        photo:pic,
         postedBy:req.user
     })
     post.save().then(result=>{
